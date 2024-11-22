b61eb2b40c2f3b41009a530c58fa3523256d2fc7
waffleprinter <dwamsie@gmail.com>
2024-11-21 23:15:41 -0500

    added getters and setters

--- src/main/java/Book.java
+++ src/main/java/Book.java
@@ -80,5 +80,15 @@ public class Book {
         isbn = book.isbn;
     }
 
-    // GETTERS AND SETTERS
+    public String getTitle() { return title; }
+    public String getAuthor() { return author; }
+    public double getPrice() { return price; }
+    public String getPublisher() { return publisher; }
+    public String getIsbn() { return isbn; }
+
+    public void setTitle(String title) { this.title = title; }
+    public void setAuthor(String author) { this.author = author; }
+    public void setPrice(double price) { this.price = price; }
+    public void setPublisher(String publisher) { this.publisher = publisher; }
+    public void setIsbn(String isbn) { this.isbn = isbn; }
 }
