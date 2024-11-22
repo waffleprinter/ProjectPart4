54e4977d3a90e6f9b527d68030b0ee934f28e244
waffleprinter <dwamsie@gmail.com>
2024-11-21 22:53:28 -0500

    added class structure

--- src/main/java/Book.java
+++ src/main/java/Book.java
@@ -1,3 +1,45 @@
 public class Book {
+    private String title;
+    private String author;
+    private double price;
+    private String publisher;
+    private String isbn;
 
+    Book() {
+
+    }
+
+    Book(String title) {
+
+    }
+
+    Book(String title, String author, String price, String publisher, String isbn) {
+
+    }
+
+    Book(Book book) {
+
+    }
+
+    public void checkIsbnStatus() {
+
+    }
+
+    public void toTitleCase() {
+
+    }
+
+    public String toString() {
+        return "";
+    }
+
+    public boolean equals(Book book) {
+        return false;
+    }
+
+    public void clone(Book book) {
+
+    }
+
+    // GETTERS AND SETTERS
 }
