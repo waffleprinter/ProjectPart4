d496e2e23b7c47c888e5a0f6388278dd4ef0b12b
waffleprinter <dwamsie@gmail.com>
2024-11-21 23:13:32 -0500

    added default constructor, constructor with title, constructor will all parameters, copy constructor, toString(), equals(), and clone() implementation.

--- src/main/java/Book.java
+++ src/main/java/Book.java
@@ -6,19 +6,35 @@ public class Book {
     private String isbn;
 
     Book() {
-
+        title = "";
+        author = "";
+        price = 0.0;
+        publisher = "";
+        isbn = "";
     }
 
     Book(String title) {
-
+        this.title = title;
+        author = "";
+        price = 0.0;
+        publisher = "";
+        isbn = "";
     }
 
-    Book(String title, String author, String price, String publisher, String isbn) {
-
+    Book(String title, String author, double price, String publisher, String isbn) {
+        this.title = title;
+        this.author = author;
+        this.price = price;
+        this.publisher = publisher;
+        this.isbn = isbn;
     }
 
     Book(Book book) {
-
+        this.title = book.title;
+        this.author = book.author;
+        this.price = book.price;
+        this.publisher = book.publisher;
+        this.isbn = book.isbn;
     }
 
     public void checkIsbnStatus() {
@@ -30,15 +46,38 @@ public class Book {
     }
 
     public String toString() {
-        return "";
+        String template =
+                """
+                %-10s: %s%n
+                %-10s: %s%n
+                %-10s: %d%n
+                %-10s: %s%n
+                %-10s: %s%n
+                """;
+
+        return String.format(template,
+                "Title", title,
+                "Author", author,
+                "Price", price,
+                "Publisher", publisher,
+                "ISBN", isbn);
     }
 
     public boolean equals(Book book) {
-        return false;
+        return (title.equals(book.title)         &&
+                author.equals(book.author)       &&
+                price == book.price              &&
+                publisher.equals(book.publisher) &&
+                isbn.equals(book.isbn)
+        );
     }
 
     public void clone(Book book) {
-
+        title = book.title;
+        author = book.author;
+        price = book.price;
+        publisher = book.publisher;
+        isbn = book.isbn;
     }
 
     // GETTERS AND SETTERS
