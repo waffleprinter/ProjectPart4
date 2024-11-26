4520366026c5b1326076c996cdb2f3f4d2f345a6
waffleprinter <jccpainchaud@gmail.com>
2024-11-26 17:22:45 -0500

    Added comments. Methods now create copies of the libraries and books passed through arguments to avoid unintended changes.

--- src/main/java/Library.java
+++ src/main/java/Library.java
@@ -1,22 +1,49 @@
 import java.util.ArrayList;
 
+/**
+ @author Jacques-Conrad Calagui-Painchaud 6298805
+ This class defines a library with a list of books.
+ */
 public class Library {
     ArrayList<Book> books = new ArrayList<>();
 
+    /**
+     Constructor
+     Creates a library with 5 blank books.
+     */
     Library() {
         for (int i = 0; i < 5; i++) {
             books.add(new Book());
         }
     }
 
+    /**
+     Constructor
+     @param books arrayList of book objects.
+     Creates a library with user-defined books.
+     */
     Library(ArrayList<Book> books) {
-        this.books = books;
+        for (Book book : books) {
+            this.books.add(new Book(book));
+        }
     }
 
+    /**
+     Copy constructor
+     Creates a library with identical members to another library.
+     */
     Library(Library otherLibrary) {
-        this.books = otherLibrary.books;
+        for (Book book : otherLibrary.books) {
+            this.books.add(new Book(book));
+        }
     }
 
+    /**
+     The searchBooks method
+     @param keyword The word that you are searching for.
+     @return List of books with the word that you are searching for.
+     Searches the library for books with the keyword in the title or in the author name.
+     */
     public ArrayList<Book> searchBooks(String keyword) {
         ArrayList<Book> filteredBooks = new ArrayList<>();
 
@@ -25,18 +52,26 @@ public class Library {
             boolean authorContainsKeyword = book.getAuthor().toLowerCase().contains(keyword.toLowerCase());
 
             if (titleContainsKeyword || authorContainsKeyword) {
-                filteredBooks.add(book);
+                filteredBooks.add(new Book(book));
             }
         }
 
         return filteredBooks;
     }
 
+    /**
+     The equals method
+     @param otherLibrary another library object.
+     @return Whether both libraries contain identical information.
+     Checks if all the books in the other library are equal to this library's books.
+     */
     public boolean equals(Library otherLibrary) {
+        // Both libraries can't be equal if they don't contain the same amount of books.
         if (books.size() != otherLibrary.books.size()) {
             return false;
         }
 
+        // Checks if all books in both libraries are equal.
         for (int i = 0; i < books.size(); i++) {
             if (!books.get(i).equals(otherLibrary.books.get(i))) {
                 return false;
@@ -46,6 +81,10 @@ public class Library {
         return true;
     }
 
+    /**
+     The toString method
+     @return A string containing the information about every book.
+     */
     public String toString() {
         String string = "Books in Library:\n";
 
